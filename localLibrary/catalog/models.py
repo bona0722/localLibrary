624a13281ed45327d2d623965044e58585031d24
bona0722 <bona0722@github.com>
2020-06-02 22:02:49 +0900

    part4

--- localLibrary/catalog/models.py
+++ localLibrary/catalog/models.py
@@ -59,6 +59,12 @@ class Book(models.Model):
         """String for representing the Model object."""
         return self.title
 
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
+
 
 import uuid  # Required for unique book instances
 from datetime import date
